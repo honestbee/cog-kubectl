182a2fb65d2277a450cda2c1bfa55aacd40c0328
Vincent De Smet <vincent.desmet@honestbee.com>
2017-05-22 01:24:34 +1000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (
     name = "kubectl",
-    version = "0.0.1",
+    version = "0.0.2",
     description = "Cog commands for kubectl",
     author = "Vincent De Smet",
     author_email = "vincent.desmet@honestbee.com",
